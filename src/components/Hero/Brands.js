78dcd8645bfa7467ec0a7c1ab6d35f1615f66621
parsariaz <parsa.riaz@arhamsoft.com>
2023-12-18 11:38:43 +0500

    modifying lottie animation and code reviewing

--- src/components/Hero/Brands.js
+++ src/components/Hero/Brands.js
@@ -1,56 +1,35 @@
-import amazon from "../../assets/hero/brands-amazon.png";
-import lg from "../../assets/hero/brands-LG.png";
-import megmeet from "../../assets/hero/brands-megmeet.png";
-import chimei from "../../assets/hero/brands-chimei.png";
-import waste from "../../assets/hero/brands-waste.png";
+import { useState } from "react";
+import brandsData from "../../data/brandsData";
 
 const Brands = () => {
+  const [isHovered, setIsHovered] = useState(0);
+
+  const handleHovered = (index) => {
+    setIsHovered(index);
+  };
+
   return (
     <div className="py-8 md:py-2 h-auto text-center flex justify-center items-center md:text-start font-f-outfit">
-     <div className="lg:w-100 xl:w-[93%]">
-      <p className="pt-2 font-light">Who endorse our work</p>
-      <div className="flex flex-col md:flex-row  gap-3 h-[60%] items-center">
-        <h2 className="font-semibold text-[2rem] whitespace-nowrap lg:text-[3rem]">
-          Trusted By:
-        </h2>
-        <ul className="flex flex-col gap-10 md:gap-4 lg:gap-10 md:flex-row w-full md:justify-evenly items-center">
-          <li className="w-[80%] md:w-[15%]">
-            <img
-              src={amazon}
-              alt="amazon logo"
-              className="object-contain w-full h-full"
-            />
-          </li>
-          <li className="w-[50%] md:w-[10%] grayscale hover:grayscale-0">
-            <img
-              src={lg}
-              alt="LG logo"
-              className="object-contain w-100 h-100"
-            />
-          </li>
-          <li className="h-[80%] md:w-[15%] grayscale hover:grayscale-0">
-            <img
-              src={megmeet}
-              alt="meegmeet logo"
-              className="object-contain w-100 h-100"
-            />
-          </li>
-          <li className="h-[80%] md:w-[15%] grayscale hover:grayscale-0">
-            <img
-              src={chimei}
-              alt="chimei logo"
-              className="object-contain w-100 h-100"
-            />
-          </li>
-          <li className="h-[80%] md:w-[15%] grayscale hover:grayscale-0">
-            <img
-              src={waste}
-              alt="waste logo"
-              className="object-contain w-100 h-100"
-            />
-          </li>
-        </ul>
-      </div>
+      <div className="lg:w-100 xl:w-[93%]">
+        <p className="pt-2 font-light">Who endorse our work</p>
+        <div className="flex flex-col md:flex-row  gap-3 h-[60%] items-center">
+          <h2 className="font-semibold text-[2rem] whitespace-nowrap lg:text-[3rem]">
+            Trusted By:
+          </h2>
+          <ul className="flex flex-col gap-10 md:gap-4 lg:gap-10 md:flex-row w-full md:justify-evenly items-center">
+            {brandsData.map((brand, index) => {
+              return (
+                <li onMouseEnter={()=>handleHovered(index)} onMouseLeave={()=>handleHovered(0)} key={index} className={`${index===0?"md:w-[15%]":"md:w-[10%]"} w-[50%] ${isHovered===index?"hover:grayscale-0":"grayscale"}`}>
+                  <img
+                    src={brand.img}
+                    alt="LG logo"
+                    className="object-contain w-100 h-100"
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
---
+++ src/data/brandsData.js
@@ -0,0 +1,25 @@
+import amazon from "../assets/hero/brands-amazon.png";
+import lg from "../assets/hero/brands-LG.png";
+import megmeet from "../assets/hero/brands-megmeet.png";
+import chimei from "../assets/hero/brands-chimei.png";
+import waste from "../assets/hero/brands-waste.png";
+
+const brandsData = [
+  {
+    img: amazon,
+  },
+  {
+    img: lg,
+  },
+  {
+    img: megmeet,
+  },
+  {
+    img: chimei,
+  },
+  {
+    img: waste,
+  },
+];
+
+export default brandsData;
